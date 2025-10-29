ae65186dc7c9ccb3951e61611c54e299129fc79e
Henrique Soubhia <henrique.soubhia@fiap.com.br>
2025-10-29 19:57:07 -0300

    remove comments

--- src/main/java/com/bip/sistema/repository/ConsumptionRepository.java
+++ src/main/java/com/bip/sistema/repository/ConsumptionRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConsumptionRepository extends JpaRepository<ConsumptionRecord, Long> {
-    // Você pode adicionar métodos personalizados, se quiser, por exemplo:
-    // List<ConsumptionRecord> findByUserId(Long userId);
-    // List<ConsumptionRecord> findByItemId(Long itemId);
+
 }
--- src/main/java/com/bip/sistema/repository/UserRepository.java
+++ src/main/java/com/bip/sistema/repository/UserRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    // busca por crachá (badge)
     User findByBadgeCode(Long badgeCode);
 }
